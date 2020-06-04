429a373100680cd5e9fa9f218b64bd9a69fcd82a
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-06-05 02:02:04 +0900

    onsenui 프레임워크 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React, { useEffect } from "react";
-import { Helmet } from "react-helmet";
-import { Button } from "react-onsenui";
-import "./App.css";
+// import { Helmet } from "react-helmet";
+import ons from "onsenui";
+import { Page, Button } from "react-onsenui";
+
 // Webpack CSS import
 import "onsenui/css/onsenui.css";
 import "onsenui/css/onsen-css-components.css";
+import "./App.css";
 
 export default function App() {
+  const handleClick = () => {
+    ons.notification.alert("네이티브 스타일 알림 메시지!");
+  };
   useEffect(() => {
     console.log("안녕하신가요?");
   }, []);
   return (
-    <div>
-      <Helmet>
-        <title>동적변경</title>
-      </Helmet>
-      <button type="button">버튼 렌더링</button>
-      <div className="test">이것은 글씨입니다.</div>
-      <img src={require("./images/img1.jpeg").default} alt="그림" />
-      <br />
-      <Button>온센 UI 버튼 렌더링</Button>
-    </div>
+    <Page>
+      <div>글씨 출력</div>
+      <div>
+        <img
+          src={require("./images/img1.jpeg").default}
+          alt="이미지"
+          style={{
+            width: "100%",
+            maxWidth: "423px",
+            height: "auto",
+          }}
+        />
+      </div>
+      <Button onClick={handleClick}>클릭해보세요!</Button>
+    </Page>
   );
 }
