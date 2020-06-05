71e8be607bcdff4c1afc93fa798128ad1f976090
Jongkeun Choi <jongkeun.ch@gmail.com>
2020-06-05 09:37:54 +0900

    grommet css framework 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,107 @@
-import React, { useEffect } from "react";
-// import { Helmet } from "react-helmet";
-import ons from "onsenui";
-import { Page, Button } from "react-onsenui";
+import React, { useState } from "react";
+import "normalize.css";
+import {
+  Grommet,
+  ThemeType,
+  Box,
+  Button,
+  Heading,
+  Collapsible,
+  ResponsiveContext,
+  Layer,
+} from "grommet";
+import { Notification, FormClose } from "grommet-icons";
 
-// Webpack CSS import
-import "onsenui/css/onsenui.css";
-import "onsenui/css/onsen-css-components.css";
-import "./App.css";
+const theme: ThemeType = {
+  global: {
+    colors: {
+      brand: "#228BE6",
+    },
+    font: {
+      family: "NanumGothic",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
 
-export default function App() {
-  const handleClick = () => {
-    ons.notification.alert("네이티브 스타일 알림 메시지!");
-  };
-  useEffect(() => {
-    console.log("안녕하신가요?");
-  }, []);
+const AppBar = (props: any) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background={{ color: "brand" }}
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: 1 }}
+    {...props}
+  />
+);
+
+export default function App(): JSX.Element {
+  const [showSidebar, setShowSidebar] = useState<boolean>(false);
   return (
-    <Page>
-      <div>글씨 출력</div>
-      <div>
-        <img
-          src={require("./images/img1.jpeg").default}
-          alt="이미지"
-          style={{
-            width: "100%",
-            maxWidth: "423px",
-            height: "auto",
-          }}
-        />
-      </div>
-      <Button onClick={handleClick}>클릭해보세요!</Button>
-    </Page>
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {(size) => (
+          <Box fill>
+            <AppBar>
+              <Heading level="3" margin="none">
+                사용자를 현혹시키는 제목
+              </Heading>
+              <Button
+                icon={<Notification />}
+                onClick={() => {
+                  setShowSidebar(!showSidebar);
+                }}
+              />
+            </AppBar>
+            <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+              <Box flex align="center" justify="center">
+                사용자를 현혹시키는 내용
+              </Box>
+              {!showSidebar || size !== "small" ? (
+                <Collapsible direction="horizontal" open={showSidebar}>
+                  <Box
+                    flex
+                    width="medium"
+                    background="light-2"
+                    elevation="small"
+                    align="center"
+                    justify="center"
+                  >
+                    사용자를 현혹시키는 알림
+                  </Box>
+                </Collapsible>
+              ) : (
+                <Layer>
+                  <Box
+                    background="light-2"
+                    tag="header"
+                    justify="end"
+                    align="center"
+                    direction="row"
+                  >
+                    <Button
+                      icon={<FormClose />}
+                      onClick={() => setShowSidebar(false)}
+                    />
+                  </Box>
+                  <Box
+                    fill
+                    background="light-2"
+                    align="center"
+                    justify="center"
+                  >
+                    사용자를 현혹시키는 모바일 알림
+                  </Box>
+                </Layer>
+              )}
+            </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
   );
 }
